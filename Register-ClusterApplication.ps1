a5e81391511dfe104ca821f984d26b695d04992e
Sebastian <me@seb2k.de>
2018-01-18 18:03:29 +0100

    Create Register-ClusterApplication.ps1

---
+++ Register-ClusterApplication.ps1
@@ -0,0 +1,4 @@
+.\Helper\MicrosoftAzureServiceFabric-AADHelpers\AADTool\SetupApplications.ps1 `
+-TenantId '' `
+-WebApplicationReplyUrl 'https://asfgraefdev.westeurope.cloudapp.azure.com:19080/Explorer/index.html' `
+-ClusterName 'asfgraefdev'
