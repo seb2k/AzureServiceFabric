024d7ead4b5e341070be3f75ca18a13037389cc9
Sebastian Gräf <sebastian.graef@de.ey.com>
2018-01-22 15:21:23 +0100

    Adding ServiceFabric

---
+++ ServiceFabric/psDeploy.ps1
@@ -0,0 +1,26 @@
+Select-AzureRmSubscription -SubscriptionId ""
+#New-AzureRmResourceGroup -Name RGServiceFabric -Location northcentralus
+
+$clusterName = "asfgraefdev"
+$clusterLocation = "westeurope"
+$sourceVaultValue = ""
+$certificateThumbprint = ""
+$certificateUrlValue = ""
+$reverseProxyCertificateThumbprint = ""
+$reverseProxyCertificateUrlValue = ""
+
+$password = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("Q2FsYW11Y2hpdGEyMDE3"))
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['clusterName'] = $clusterName
+$additionalParameters['clusterLocation'] = $clusterLocation
+
+$additionalParameters['adminPassword'] = $securePassword
+
+$additionalParameters['sourceVaultValue'] = $sourceVaultValue
+$additionalParameters['certificateThumbprint'] = $certificateThumbprint
+$additionalParameters['certificateUrlValue'] = $certificateUrlValue
+$additionalParameters['reverseProxyCertificateThumbprint'] = $reverseProxyCertificateThumbprint
+$additionalParameters['reverseProxyCertificateUrlValue'] = $reverseProxyCertificateUrlValue
+
+New-AzureRmResourceGroupDeployment -Name "$clusterName-deployment" -ResourceGroupName RGServiceFabric -TemplateFile .\azuredeploy.json @additionalParameters -Verbose -Force
